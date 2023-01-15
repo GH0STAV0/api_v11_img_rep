328de95365465857ccd7fce490d92773488df81a
GH0STAV0 <you@example.com>
2023-01-14 19:59:32 -0500

    docker with node

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message:  "Version : V-10 [ 14 01 2023 ] YH84 \n PUT /google_account/activate/:customerId5 ; /google_account/update/:customerId4 ; GET  /van/1 ;   GET /van/ran ; GET  /van/count   ; PUT /van/reset_all ; PUT /van/update/1  ; GET /van/ran ; GET  /van/count   ; GET /google_account/active ; GET  /nor/1 ; /pure/ran GET  /nor/count  ,  GET /nor/ran ; GET  /nor/count   ; PUT /nor/reset_all ; PUT /nor/update/1 " });
+});
+
+require("./app/routes/customer.routes.js")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
